78bbcbd31940d8c12faff8adc1cf751429f6e917
Brielle Petrie <brielle.petrie@gmail.com>
2020-07-30 19:00:42 -0600

    UI improvements, game functionality

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const gameContainer = document.getElementById('game');
+const newGameBtn = document.getElementById('new_game');
+const showScore = document.getElementById('current_score');
+const barnUrl = 'https://cdn.pixabay.com/photo/2014/03/25/16/58/barn-297763_960_720.png';
 
 const COLORS = [ 'red', 'blue', 'green', 'orange', 'purple', 'red', 'blue', 'green', 'orange', 'purple' ];
+const IMAGES = [
+	'https://cdn.pixabay.com/photo/2013/07/13/12/34/cow-159893_960_720.png',
+	'https://cdn.pixabay.com/photo/2018/07/16/15/31/dog-3542195_960_720.png',
+	'https://cdn.pixabay.com/photo/2016/03/31/21/04/horse-1296173_960_720.png',
+	'https://cdn.pixabay.com/photo/2018/07/15/13/29/lamb-3539619_960_720.png',
+	'https://cdn.pixabay.com/photo/2012/05/07/04/17/pig-47920_960_720.png',
+	'https://cdn.pixabay.com/photo/2013/07/13/12/34/cow-159893_960_720.png',
+	'https://cdn.pixabay.com/photo/2018/07/16/15/31/dog-3542195_960_720.png',
+	'https://cdn.pixabay.com/photo/2016/03/31/21/04/horse-1296173_960_720.png',
+	'https://cdn.pixabay.com/photo/2018/07/15/13/29/lamb-3539619_960_720.png',
+	'https://cdn.pixabay.com/photo/2012/05/07/04/17/pig-47920_960_720.png'
+];
 
+let currentScore = 0;
 let clicked = [];
 let allClickedCards = [];
 let result;
@@ -29,18 +45,18 @@ function shuffle(array) {
 	return array;
 }
 
-let shuffledColors = shuffle(COLORS);
+let shuffledImgs = shuffle(IMAGES);
 
 // this function loops over the array of colors
 // it creates a new div and gives it a class with the value of the color
 // it also adds an event listener for a click for each card
-function createDivsForColors(colorArray) {
-	for (let color of colorArray) {
+function createDivsForColors(imgArray) {
+	for (let img of imgArray) {
 		// create a new div
 		const newDiv = document.createElement('div');
 
 		// give it a class attribute for the value we are looping over
-		newDiv.classList.add(color);
+		newDiv.classList.add(img);
 
 		// call a function handleCardClick when a div is clicked on
 		newDiv.addEventListener('click', handleCardClick);
@@ -63,16 +79,20 @@ function handleCardClick(event) {
 	if (clicked.length === 0) {
 		clicked.push(clickedCard);
 		allClickedCards.push(clickedCard);
-		clickedCard.style.backgroundColor = clickedCard.classList[0];
+		clickedCard.style.backgroundImage = `url(${clickedCard.classList[0]})`;
 		// handle the second of the two clicks
 	} else if (clicked.length === 1) {
 		clicked.push(clickedCard);
 		allClickedCards.push(clickedCard);
-		clickedCard.style.backgroundColor = clickedCard.classList[0];
-		result = checkMatch(clicked[0].style.backgroundColor, clicked[1].style.backgroundColor);
+		clickedCard.style.backgroundImage = `url(${clickedCard.classList[0]})`;
+		result = checkMatch(clicked[0].style.backgroundImage, clicked[1].style.backgroundImage);
 		if (result === true) {
 			// start the counter over
+			for (card of clicked) {
+				card.classList.add('glow');
+			}
 			clicked = [];
+			updateGuesses();
 			// keep from re-clicking the card, take it out of the running
 		} else {
 			setTimeout(function() {
@@ -81,12 +101,23 @@ function handleCardClick(event) {
 			}, 1000);
 			allClickedCards.pop();
 			allClickedCards.pop();
+			updateGuesses();
 		}
+	}
+
+	if (allClickedCards.length === COLORS.length) {
+		setTimeout(function() {
+			gameOver();
+		}, 100);
 	}
 }
 
+newGameBtn.addEventListener('click', function() {
+	newGame();
+});
+
 // when the DOM loads
-createDivsForColors(shuffledColors);
+createDivsForColors(shuffledImgs);
 
 function checkMatch(item1, item2) {
 	return item1 === item2;
@@ -94,13 +125,26 @@ function checkMatch(item1, item2) {
 
 function flipCards(arr) {
 	for (let item of arr) {
-		item.style.backgroundColor = 'white';
+		item.style.backgroundImage = 'url(https://cdn.pixabay.com/photo/2014/03/25/16/58/barn-297763_960_720.png)';
 	}
 }
 
+function updateGuesses() {
+	currentScore++;
+	showScore.innerText = currentScore;
+}
+
 function newGame() {
-	flipCards(allClickedCards);
+	gameContainer.innerHTML = '';
 	allClickedCards = [];
 	clicked = [];
-	console.log(shuffle(COLORS));
+	currentScore = 0;
+	showScore.innerText = currentScore;
+	createDivsForColors(shuffle(IMAGES));
+	newGameBtn.classList.remove('glow');
+}
+
+function gameOver() {
+	newGameBtn.classList.add('glow');
+	alert('You win!');
 }
